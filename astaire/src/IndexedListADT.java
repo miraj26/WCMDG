ae0190eead6e735e34eaddb80787f4a8bab816a7
Miraj <miraj.shah@hotmail.co.uk>
2018-11-13 15:09:00 +0000

    implemented indexedlist adt

--- astaire/src/IndexedListADT.java
+++ astaire/src/IndexedListADT.java
@@ -2,4 +2,12 @@
 public interface IndexedListADT<T> extends ListADT<T>{
 
 	public T get(int i);
+	
+	public T set(int i, T element);
+	
+	public T remove(int i);
+	
+	public void add(int i, T element);
+	
+	public int indexOf(T element);
 }
